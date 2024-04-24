fba1b1f87343163da386f696208dcf571a8c1519
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-04-24 20:05:15 +0600

    get chat from firebase using stream

--- lib/components/chat_messages.dart
+++ lib/components/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +6,26 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('No Messages Found'),);
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').orderBy('createdAt').snapshots(),
+      builder: (BuildContext context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData) {
+          return const Center(
+            child: Text('No Messages Found'),
+          );
+        }
+        if (snapshot.hasError) {
+          return const Center(child: Text("something went wong...."));
+        }
+        final data = snapshot.data!.docs;
+        return ListView.builder(itemCount:data.length, itemBuilder: (BuildContext context, int index) { 
+          return Text(data[index].data()['text']);
+        }, );
+      },
+    );
   }
 }
