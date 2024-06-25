ba56f7dc65e28b906593642d88a82a774f5be0b0
taufiq <taufiq.elahi.a@gmail.com>
2024-06-25 15:47:57 +0600

    set up stripe for andriod and ios

--- android/app/src/main/kotlin/com/example/udemy_flutter_section14/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/udemy_flutter_section14/MainActivity.kt
@@ -2,4 +2,7 @@ package com.example.udemy_flutter_section14
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
